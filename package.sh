673c6c8d91f333cc61ec7028d37f1a6acdd10cdf
Dan Wagoner <dan.wagoner@mailprotector.com>
2022-05-17 16:38:55 -0400

    first attempt at creating and publishing release

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly CERTBOT_VERSION=$( awk -F= '$1 == "certbot"{ print $NF; }' "${SCRIPT_DIR}/requirements.txt" )
 readonly VENV="certbot/venv"
 readonly PYTHON="python3.8"
-readonly CERTBOT_ZIP_FILE="certbot-${CERTBOT_VERSION}.zip"
+readonly CERTBOT_ZIP_FILE="certbot-lambda.zip"
 readonly CERTBOT_SITE_PACKAGES=${VENV}/lib/${PYTHON}/site-packages
 
 cd "${SCRIPT_DIR}"
